e456136cc95527b4f9b1dc1c54b321c165574470
hugo <hugo.bernier88@gmail.com>
2025-06-23 11:13:36 +0200

    [DOCKERFILE] fixed dockerfile... again

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,10 @@ export default defineConfig({
         react(),
         tailwindcss(),
     ],
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+    },
     esbuild: {
         jsx: 'automatic',
     },
