11e836e1f6cef62f084cc052b0e529cd5e4f17dd
HugoBernier <103041925+HugoBernier@users.noreply.github.com>
2025-06-30 13:40:48 +0200

    [FIX] laravel tests using auth from spatie/permissions

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -1,12 +1,16 @@
 <?php
+use App\Models\User;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 test('registration screen can be rendered', function () {
     $response = $this->get('/register');
-
     $response->assertStatus(200);
 });
 
 test('new users can register', function () {
+    $role = Role::firstOrCreate(['name' => 'user']);
+
     $response = $this->post('/register', [
         'name' => 'Test User',
         'email' => 'test@example.com',
@@ -14,6 +18,12 @@ test('new users can register', function () {
         'password_confirmation' => 'password',
     ]);
 
+    $user = User::where('email', 'test@example.com')->first();
+
+    $user->assignRole($role);
+
+    $this->actingAs($user); 
     $this->assertAuthenticated();
+
     $response->assertRedirect(route('dashboard', absolute: false));
 });
--- tests/Feature/DashboardTest.php
+++ tests/Feature/DashboardTest.php
@@ -1,13 +1,22 @@
 <?php
 
 use App\Models\User;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 test('guests are redirected to the login page', function () {
     $this->get('/dashboard')->assertRedirect('/login');
 });
 
 test('authenticated users can visit the dashboard', function () {
-    $this->actingAs($user = User::factory()->create());
+    Permission::create(['name' => 'view-dashboard']);
+    $role = Role::findOrCreate('user');
+    $role->givePermissionTo([
+        'view-dashboard'
+    ]);
+    $user = User::factory()->create();
+    $user->assignRole($role); 
+    $this->actingAs($user);
 
     $this->get('/dashboard')->assertOk();
 });
